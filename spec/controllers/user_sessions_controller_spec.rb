2973f8cd26f7d0e5ef58710206599ac2e0dbf907
Alan Andrade <alan.andradec@gmail.com>
2012-08-05 14:15:48 -0700

    rspec tests

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe UserSessionsController do
+  describe 'GET#new' do
+    subject { get :new }
+    before  { subject }
+    it{ assigns(:user_session).should_not be_nil }
+  end
+
+  describe 'DELETE#destroy' do
+    context 'with an existing session' do
+      subject { delete :destroy  }
+      let(:mock_before_filter){ controller.stub(require_user: 'I dont redirect') }
+      let(:user_session_stub) { stub_model(UserSession) }
+
+      before do
+        mock_before_filter
+        user_session_stub.should_receive(:destroy)
+        controller.stub(current_user_session: user_session_stub)
+        subject
+      end
+
+      it { should redirect_to root_url }
+    end
+  end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe UsersController do
+  describe ' GET#mi_cuenta (show)' do
+    let(:stubbed_user) { stub_model(User) }
+    context 'with a logged id user' do
+      subject { get :show }
 
+      before do
+        controller.stub(require_user: stubbed_user)
+        subject
+      end
+
+      it{ response.should be_successful }
+    end
+  end
 end
